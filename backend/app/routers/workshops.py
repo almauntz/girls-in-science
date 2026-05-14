c07c4c576fbcb6d4b9a65275915d216c01547573
Amel-Tokic <amel.tokic@fet.ba>
2026-05-14 22:35:54 +0200

    dodane sheme za workshops

--- backend/app/routers/workshops.py
+++ backend/app/routers/workshops.py
@@ -1,26 +1,42 @@
+from datetime import datetime
+from typing import Optional
 from fastapi import APIRouter, Depends
+from pydantic import BaseModel
 from sqlmodel import Session
 from app.database import get_db
 from app.core.security import get_current_user
 from app.models.user import User
+from app.models.workshops_models import WorkshopStatus
 
 router = APIRouter(prefix="/workshops", tags=["workshops"])
 
-# -------------------------------------------------------
-# Team 1 — Workshops
-# This is your router. All your endpoints go here.
-# 
-# Example protected endpoint:
-#
-# @router.get("/")
-# def get_workshops(
-#     db: Session = Depends(get_db),
-#     current_user: User = Depends(get_current_user)
-# ):
-#     return {"message": "your code here"}
-#
-# -------------------------------------------------------
 
 @router.get("/")
 def workshops_placeholder():
     return {"message": "Workshops router is working — Team 1 builds here"}
+
+
+class WorkshopCreate(BaseModel):
+    title: str
+    description: str
+    location: str
+    date: datetime
+    capacity: int
+
+class WorkshopUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    location: Optional[str] = None
+    date: Optional[datetime] = None
+    capacity: Optional[int] = None
+
+class WorkshopRead(BaseModel):
+    id: int
+    title: str
+    description: str
+    location: str
+    date: datetime
+    capacity: int
+    status: WorkshopStatus
+    created_by_id: Optional[int]
+    created_at: Optional[datetime]
