f0fb0f8004b18a1939b9d1f37b3050258fb93fd3
Šejla <sejla.valjevac@fet.ba>
2026-05-15 10:31:29 +0200

    Add RoleModel model

---
+++ backend/app/models/role_model.py
@@ -0,0 +1,11 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+
+class RoleModel(SQLModel, table=true):
+    id:Optional[int]=Field(default=None, primary_key=True)
+    first_name: str
+    last_name: str
+    stem_field: str
+    institution: str
+    biography: Optional[str]=None
+    achievements: Optional[str]=None
