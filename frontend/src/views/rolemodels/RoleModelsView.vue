2f123312ea4b2b8ac431d9bd92e4b6990bf54ae4
Šejla <sejla.valjevac@fet.ba>
2026-05-15 11:47:40 +0200

    Add RoleModelsView

--- frontend/src/views/rolemodels/RoleModelsView.vue
+++ frontend/src/views/rolemodels/RoleModelsView.vue
@@ -1,6 +1,21 @@
+<script setup>
+import {ref,onMounted} from "vue"
+import RoleModelCard from "@/components/RoleModelCard.vue"
+import {getRoleModels} from "@/services/api"
+const roleModels=ref([])
+onMounted(async()=>{
+  roleModels.value=await getRoleModels()
+})
+</script>
 <template>
   <div>
-    <h1>Role Models</h1>
-    <p>Tim 3 radi ovdje.</p>
+    <h1>Women in Science</h1>
+    <div v-if="roleModels.length===0">
+      No profiles available.
+    </div>
+    <RoleModelCard v-for="model in roleModels"
+    :key="model.id"
+    :roleModel="model"
+    />
   </div>
 </template>
