07cc28183b28f1998d1052c06ce1056d946c15d4
Šejla <sejla.valjevac@fet.ba>
2026-05-15 11:36:48 +0200

    Add RoleModelCard component

---
+++ frontend/src/components/RoleModelCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card">
+        <h2>
+            {{roleModel.first_name}}
+            {{roleModel.last_name}}
+        </h2>
+        <p>{{roleModel.stem_field}}</p>
+        <p>{{roleModel.institution}}</p>
+    </div>
+
+</template>
+
+<script setup>
+define Props({
+    roleModel:Object
+})
+</script>
