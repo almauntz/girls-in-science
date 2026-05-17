ed17a9bf35e5dc09fc23c3e2aa0484763cc354db
Amel-Tokic <amel.tokic@fet.ba>
2026-05-17 19:42:27 +0200

    Dodana klasa workshop

--- backend/app/models/workshops_models.py
+++ backend/app/models/workshops_models.py
@@ -16,3 +16,14 @@ class WorkshopBase(SQLModel):
     date: datetime
     end_time: datetime
     capacity: int
+
+
+class Workshop(WorkshopBase, table=True):
+    __tablename__ = "workshops"
+
+    ID_workshop: Optional[int] = Field(default=None, primary_key=True)
+    status: WorkshopStatus = Field(default=WorkshopStatus.upcoming)
+    created_by_id: Optional[int] = Field(default=None, foreign_key="users.id")
+    created_at: Optional[datetime] = Field(
+        default_factory=lambda: datetime.now(timezone.utc)
+    )
