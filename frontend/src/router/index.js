b6a57a879674d957940798d575ffbb833c526ebb
Dženan <dzenan.cerkezovic@fet.ba>
2026-05-18 19:26:08 +0200

    fix: ispravljen redoslijed ruta, /role-models/add premještena ispred /role-models/:id

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
-
 const routes = [
   {
     path: '/',
@@ -40,32 +39,35 @@ const routes = [
     
   },
   {
-    path: '/role-models/:id',
-    name: 'role-model-detail',
-    component: () => import('../views/rolemodels/RoleModelDetail.vue')
-  },
-{
-    path: '/role-models/:id/edit',
-    name: 'rolemodels-edit',
-    component: () => import('../views/rolemodels/RoleModelEdit.vue'),
+    path: '/role-models/add',
+    name: 'rolemodels-add',
+    component: () => import('../views/rolemodels/RoleModelAdd.vue'),
     meta: { requiresAuth: true }
-},
+  },
   {
     path: '/profiles',
     name: 'profiles',
     component: () => import('../views/profiles/ProfilesView.vue'),
     meta: { requiresAuth: true }
   },
+  {
+  path: '/role-models/:id',
+  name: 'rolemodels-detail',
+  component: () => import('../views/rolemodels/RoleModelDetail.vue'),
+},
+  {
+  path: '/role-models/:id/edit',
+  name: 'rolemodels-edit',
+  component: () => import('../views/rolemodels/RoleModelEdit.vue'),
+  meta: { requiresAuth: true }
+},
 ]
-
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
-
 router.beforeEach((to, from, next) => {
   const isLoggedIn = !!localStorage.getItem('token')
-
   if (to.meta.requiresAuth && !isLoggedIn) {
     next('/login')
   } else if (to.meta.guestOnly && isLoggedIn) {
@@ -74,5 +76,4 @@ router.beforeEach((to, from, next) => {
     next()
   }
 })
-
 export default router
