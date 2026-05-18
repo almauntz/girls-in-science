244ea82961743a678ee06abef8c9a67e920a8f03
MaidaKamencic <maida.kamencic@fet.ba>
2026-05-18 15:50:57 +0200

    Dodana poruka ukoliko nema aktivnih radionica

--- frontend/src/views/workshops/WorkshopsView.vue
+++ frontend/src/views/workshops/WorkshopsView.vue
@@ -4,12 +4,16 @@
       <h1 class="text-4xl font-bold text-gray-800 mb-4">Pronađi edukativne radionice</h1>
       <p class="text-gray-500 text-lg">Izaberi radionicu i prijavi se u par klikova.</p>
     </div>
-    
     <hr class="border-gray-200" />
 
+    <!-- Lista radionica -->
     <div class="py-12 px-4">
       <h2 class="text-3xl font-bold text-center text-gray-800 mb-2">Aktivne radionice</h2>
       <p class="text-center text-gray-500 mb-10">Klikom na Saznaj više pogledajte detaljne informacije o radionici</p>
+
+     <!-- Poruka-->
+    <p v-if="error" class="text-center text-gray-500 text-lg">{{ error }}</p>
+
     </div>
   </div>
 </template>
