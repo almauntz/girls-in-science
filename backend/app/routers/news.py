0a5a3744679547c57fe2b9ad9f693ed078c840df
Amina Sarhatlic <amina.sarhatlic@fet.ba>
2026-05-27 19:42:11 +0200

    feat: implementiran GET /news endpoint bez autentifikacije

--- backend/app/routers/news.py
+++ backend/app/routers/news.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlmodel import Session
+from sqlmodel import Session, select
 from app.database import get_db
 from app.models.news import NewsPost
 
@@ -12,3 +12,9 @@ def get_news_post(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Objava nije pronađena")
     _ = news_post.role_models
     return news_post
+
+@router.get("/")
+def get_news_posts(db: Session = Depends(get_db)):
+    statement = select(NewsPost).order_by(NewsPost.created_at.desc())
+    news_posts = db.exec(statement).all()
+    return news_posts
